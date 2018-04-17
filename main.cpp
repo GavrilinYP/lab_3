28626e0e0c1feb217435067f86f418a10b2e9603
u211-13 <u211-13@public.mpei.local>
2018-04-17 10:27:58 +0300

    Last commit

--- main.cpp
+++ main.cpp
@@ -5,81 +5,7 @@
 #include <cassert>
 using namespace std;
 
-char Scale[]="CKF";
-struct Temperature{
-    char scale;
-    double temp;
-};
-istream & operator >> (istream &in, Temperature &Temp){
-    in >> Temp.temp;
-    in >> Temp.scale;}
-void test_temperature_input(){
-    Temperature Temp;
-    istringstream iss1("10C");
-    iss1>>Temp;
-    assert(Temp.temp == 10);
-    assert(Temp.scale == 'C');
-    istringstream iss2("0K");
-    iss2>>Temp;
-    assert(Temp.temp == 0);
-    assert(Temp.scale == 'K');
-    istringstream iss3("-400F");
-    iss3>>Temp;
-    assert(Temp.temp == -400);
-    assert(Temp.scale == 'F');
-}
-double convert (const Temperature &Temp, char scale_to) {
-    double T_Kel;
-    T_Kel = Temp.temp;
-    switch (Temp.scale){
-    case 'C':
-    case 'c':
-        T_Kel+=273;
-        break;
-    case 'F':
-    case 'f':
-        T_Kel=((5.0/9)*(T_Kel-32)+273);
-        break;
-    }
-    switch (scale_to){
-    case 'C':
-    case 'c':
-        T_Kel-=273;
-        break;
-    case 'F':
-    case 'f':
-        T_Kel=((9.0/5)*(T_Kel - 273)+32);
-        break;
-    }
-    return T_Kel;
-}
-bool operator< (const Temperature& lhs, const Temperature& rhs)
-{
-    return convert(lhs,'K')<convert(rhs, 'K');
-}
-bool operator> (const Temperature& lhs, const Temperature& rhs)
-{
-    return convert(lhs,'K')>convert(rhs, 'K');
-}
-Temperature operator- (const Temperature& lhs, const Temperature& rhs)
-{
-    Temperature ret_temp;
-    ret_temp.temp=convert(lhs,'K')-convert(rhs, 'K');
-    ret_temp.scale='K';
-    return  ret_temp;
-}
-Temperature operator/ (const Temperature& lhs, const Temperature& rhs)
-{
-    Temperature ret_temp;
-    ret_temp.temp=convert(lhs,'K')/convert(rhs, 'K');
-    ret_temp.scale='K';
-    return  ret_temp;
-}
-
-
-int
-main() {
-    test_temperature_input();
+int main() {
     size_t count;
    char el, probel, otstup;
    int proverka;
@@ -119,7 +45,6 @@ main() {
     cout<<"vibirite_simvol_dlya_elementa";
     cin>>el;
     proverka=el;
-    cout<<proverka;
     if (proverka==13||proverka==9)
         cout<<"ERROR";
     else{
@@ -151,6 +76,5 @@ main() {
 
                     }
                 }}}}
-    cin>>el;
     return 0;
 }
---
+++ test_input.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Temperature.h"
+#include <sstream>
+#include <cassert>
+
+using namespace std;
+int main (){
+    Temperature Temp;
+    istringstream iss1("10C");
+    iss1>>Temp;
+    assert(Temp.temp == 10);
+    assert(Temp.scale == 'C');
+    istringstream iss2("0K");
+    iss2>>Temp;
+    assert(Temp.temp == 0);
+    assert(Temp.scale == 'K');
+    istringstream iss3("-400F");
+    iss3>>Temp;
+    assert(Temp.temp == -400);
+    assert(Temp.scale == 'F');
+    istringstream iss2("0K");
+    iss4>>Temp;
+    assert(Temp.temp > 0);
+    assert(Temp.scale == 'K');
+}
